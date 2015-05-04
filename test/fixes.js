c5c3bcc276efb4e7a824f346da29026361059090
Veli Pehlivanov <bugventure@gmail.com>
2015-05-05 00:38:25 +0300

    Fix cannot dereference schema when ids change resolution scope (#14)

--- test/fixes.js
+++ test/fixes.js
@@ -32,4 +32,43 @@ describe('fixes', function () {
 
         assert(validate({ 123: true }));
     });
+
+    it('Fix cannot dereference schema when ids change resolution scope (#14)', function () {
+        var schema = {
+                $ref: '#child',
+                definitions: {
+                    child: {
+                        id: '#child',
+                        type: 'string'
+                    }
+                }
+            },
+            validate;
+
+        assert.doesNotThrow(function () {
+            validate = jsen(schema);
+        });
+
+        assert(validate('abc'));
+        assert(!validate(123));
+
+        schema = {
+            $ref: '#child/definitions/subchild',
+            definitions: {
+                child: {
+                    id: '#child',
+                    definitions: {
+                        subchild: {
+                            type: 'number'
+                        }
+                    }
+                }
+            }
+        };
+
+        assert.throws(function () {
+            // cannot dereference a URI, part of which is an ID
+            validate = jsen(schema);
+        });
+    });
 });
